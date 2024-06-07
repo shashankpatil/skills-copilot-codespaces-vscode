82941a9a63f00649195e6e6554f5542d6a8d782c
Shashank Patil <shashank7977@gmail.com>
2024-06-07 08:06:52 +0000

    copilot third commit bug fix33

--- comments.js
+++ comments.js
@@ -1,15 +1,1 @@
-// Create a Web Server
-// Importing the http module 
-const http = require("http") 
-
-// Creating server 
-const server = http.createServer((req, res) => { 
-	// Sending the response 
-	res.write("This is the response from the server") 
-	res.end(); 
-}) 
-
-// Server listening to port 3000 
-server.listen((3000), () => { 
-	console.log("Server is Running"); 
-})
+// Create a Web Server with Node.js
